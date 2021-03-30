4455f344e41a6491a8bb3ed5ae3fec2f99f0c773
Marc Kohaupt <debuglevel@gmail.com>
2021-03-30 23:11:33 +0200

    Add script to upload dumped postalcodes

--- supplemental/post-postalcodes.sh
+++ supplemental/post-postalcodes.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # usage:
-# curl -o backup.json http://localhost:8080/postalcodes/
+# curl -o postalcodes.json http://localhost:8080/postalcodes/
 # cat postalcodes.json | post-postalcodes.sh
 
 BASE_URL=http://localhost:8080
 
-args=( -d @- -H "Content-Type: application/json" )
+args=( -s -S --output /dev/null -d @- -H "Content-Type: application/json" )
 
 while IFS= read -r value; do
-  echo "Processing: $value" >&2
-  curl "${args[@]}" $BASE_URL/postalcodes/ <<< "$value"
+  #echo "Processing: $value" >&2
+  #curl "${args[@]}" $BASE_URL/postalcodes/ <<< "$value"
+  curl "${args[@]}" $BASE_URL/postalcodes/ <<< "$value" & echo test
 done < <( jq --compact-output '.[] | { code: .code, centerLatitude:.centerLatitude, centerLongitude:.centerLongitude, note:.note }' )
