0ab95f2520679744d21d6b2b87b05e639ad3fdee
Marc Kohaupt <debuglevel@gmail.com>
2021-12-04 18:46:04 +0100

    Fix @Id for MariaDB

--- src/main/kotlin/de/debuglevel/streetlister/street/Street.kt
+++ src/main/kotlin/de/debuglevel/streetlister/street/Street.kt
@@ -2,16 +2,23 @@ package de.debuglevel.streetlister.street
 
 import io.micronaut.data.annotation.DateCreated
 import io.micronaut.data.annotation.DateUpdated
+import org.hibernate.annotations.GenericGenerator
 import java.time.LocalDateTime
 import java.util.*
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
 data class Street(
+    /**
+     * @implNote: Needs @GeneratedValue(generator = "uuid2"), @GenericGenerator and @Column to work with MariaDB/MySQL. See https://github.com/micronaut-projects/micronaut-data/issues/1210
+     */
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
     var id: UUID?,
     /**
      * The postal code
